c16eda5ef8b87e2711bb83ed11fc09773ff585c2
yokotsuka@MBP <yokotsuka@infocity.co.jp>
2019-01-31 14:08:47 +0900

    Update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 const {performance} = require('perf_hooks');
-module.exports = (func = () => {}, name = "") => {
+module.exports = (func = () => {},logOutFunc = () => {},name = "") => {
   const startTime = performance.now();
   func();
   const t = Math.round(performance.now() - startTime);
-  console.log(`${name}${name && "="}${t}msec`);
+  logOutFunc(`${name}${name && "="}${t}msec`);
 };
