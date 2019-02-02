453226e6e442f0cd18b318926ab1ffd30a209ea2
yokotsuka@MBP <yokotsuka@infocity.co.jp>
2019-02-02 17:20:01 +0900

    delete /index.js and update version

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const {performance} = require('perf_hooks');
-module.exports = (func = () => {},logOutFunc = () => {},name = "") => {
-  const startTime = performance.now();
-  func();
-  const t = Math.round(performance.now() - startTime);
-  logOutFunc(`${name}${name && "="}${t}msec`);
-};
